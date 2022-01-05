8aca708891bf2eb6a4d9ffb13034b34974c1afc6
kcap17 <abhishek91666@icloud.com>
2022-01-05 20:56:30 +0530

    assignment 1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,20 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    return process.argv[process.argv.length - 1];
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name;
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    let readline =  require('readline');
+    let read = readline.createInterface(process.stdin,process.stdout);
+    read.question("Enter Your Full Name",(fullname) => {
+        console.log(fullname);
+    });
 }
 
 module.exports = {
